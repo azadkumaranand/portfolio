5fffee2d9fe01f1be4a06f1a7b7e7c9b8c269e5a
azadkumaranand <azadanand9798@gmail.com>
2022-08-28 05:07:46 +0530

    add member
    I have added a new member devansh Nagar

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
+const navbar = document.querySelector('.navbar'),
+navbarspan = document.querySelector('.logo strong span')
+
+window.onscroll =  ()  =>{
+    // called when the window is scrolled.
+    if (window.scrollY >= 100) {
+        navbar.style.backgroundColor = "crimson"
+        navbarspan.style.color = "white";
+    }
+    else if(window.scrollY==0) {
+        navbar.style.backgroundColor = "transparent"
+        navbarspan.style.color = "crimson";
+    }
+}
+
 const menubar = document.getElementById('menubar');
 const menu = document.getElementById('menu');
-menubar.addEventListener('click',()=>{
-    if(menu.style.display == "none"){
+menubar.addEventListener('click', () => {
+    if (menu.style.display == "none") {
         menu.style.display = "block";
         menubar.innerHTML = `<img src="xmarks.png">`
     }
-    else{
+    else {
         menu.style.display = "none"
         menubar.innerHTML = `<img src="bars.png">`
     }
 })
+
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Desiner", "Freelacer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing1", {
+    strings: ["YouTuber", "Desiner", "Freelacer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
